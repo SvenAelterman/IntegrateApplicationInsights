4514cf33e0ab01a6e2448ae2ec7ea12dc9ee87b7
Sven Aelterman <github@aelterman.com>
2016-08-19 16:56:27 -0500

    Add and implement ApiClient Implement Web API Get methods

---
+++ AppToAzureIn60Minutes/ApiClient/MyGreatTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiClient
+{
+	public class MyGreatTable
+	{
+		public int MyGreatTableId { get; set; } 
+		public string GreatField { get; set; } 
+	}
+}
