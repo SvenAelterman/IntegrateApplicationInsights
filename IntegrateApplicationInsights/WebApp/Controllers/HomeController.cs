3f75e003bd0b153881f52e811a8cb41c1f89b0f8
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2017-07-20 17:02:45 -0500

    Add custom events and measures

--- IntegrateApplicationInsights/WebApp/Controllers/HomeController.cs
+++ IntegrateApplicationInsights/WebApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.ApplicationInsights;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,25 +7,46 @@ using System.Web.Mvc;
 
 namespace WebApp.Controllers
 {
-	public class HomeController : Controller
-	{
-		public ActionResult Index()
-		{
-			return View();
-		}
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var MyMetrics = new Dictionary<string, double>
+            {
+                { "Measure 1", DateTime.UtcNow.Hour },
+                { "Measure 2", DateTime.UtcNow.Second }
+            };
+            new TelemetryClient().TrackEvent(nameof(Index), null, MyMetrics);
 
-		public ActionResult About()
-		{
-			ViewBag.Message = "Integrate Application Insights";
+            return View();
+        }
 
-			return View();
-		}
+        public ActionResult About()
+        {
+            ViewBag.Message = "Integrate Application Insights";
 
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Integrate Application Insights";
+            var MyMetrics = new Dictionary<string, double>
+            {
+                { "Measure 1", DateTime.UtcNow.Minute },
+                { "Measure 2", DateTime.UtcNow.Second }
+            };
+            new TelemetryClient().TrackEvent(nameof(About), null, MyMetrics);
 
-			return View();
-		}
-	}
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            var MyMetrics = new Dictionary<string, double>
+            {
+                { "Measure 1", DateTime.UtcNow.Hour },
+                { "Measure 2", DateTime.UtcNow.Second }
+            };
+            new TelemetryClient().TrackEvent(nameof(Contact), null, MyMetrics);
+
+            ViewBag.Message = "Integrate Application Insights";
+
+            return View();
+        }
+    }
 }
